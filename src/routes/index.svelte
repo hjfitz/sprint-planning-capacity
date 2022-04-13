73ab64168cef391b69e1ea0144ebd671578402d1
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2022-04-13 10:51:45 +0100

    initial functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<template>
+	<header>
+		<ul class="grid grid-cols-5 my-2 gap-2">
+			<li class="text-xl">Developer</li>
+			<li class="text-xl">Days Spent Working</li>
+			<li class="text-xl">Days Holiday</li>
+			<li class="text-xl">Days With Other Commitments</li>
+			<li class="text-xl">Days for Development</li>
+		</ul>
+	</header>
+	<ul>
+		{#each developers as dev, i}
+		<li class="grid grid-cols-5 gap-2 my-2">
+			<input bind:value={dev.name}/>
+			<input type=number bind:value={dev.daysWorking} on:input={() => recalculateDevDays(i)} />
+			<input type=number bind:value={dev.daysHoliday} on:input={() => recalculateDevDays(i)} />
+			<input type=number bind:value={dev.daysOtherCommitment} on:input={() => recalculateDevDays(i)} />
+			<p> {dev.devDays} </p>
+		</li>
+		{/each}
+	</ul>
+
+	<p>Total dev days: {totalDevDays}</p>
+	<p>Total with buffer: {totalWithBuffer}</p>
+	<p>Are we on support? <button on:click={toggleSupport}>{support ? 'yes' : 'no'}</button></p>
+	<p class={support ? '' : 'hidden'}>Total with support buffer: {totalWithSupportBuffer}</p> 
+	<p>Average points per dev day: <input type=number bind:value={avgPoints} /></p>
+	<p>Guideline sprint points: {avgPoints * totalWithSupportBuffer}</p>
+
+</template>
+<script lang="ts">
+	interface Developer {
+		name: string
+		daysHoliday: number
+		daysOtherCommitment: number
+		daysWorking: number
+		devDays: number
+		notes?: string
+	}
+
+	function recalculateDevDays(idx: number) {
+		const dev = developers[idx]
+		dev.devDays = dev.daysWorking - (dev.daysOtherCommitment + dev.daysHoliday)
+		// needed for reactivity
+		developers = developers
+	}
+
+	function developerFactory(): Developer {
+		return {
+			name: 'Developer',
+			daysHoliday: 0,
+			daysOtherCommitment: 0,
+			daysWorking: 8,
+			notes: undefined,
+			devDays: 8
+		}
+	}
+
+	function toggleSupport() {
+		support = !support
+	}
+
+	let avgPoints = 1
+	let support = false
+	let developers = Array.from({length: 5}, developerFactory)
+
+	$: totalDevDays = developers.map(dev => dev.devDays).reduce((acc, cur) => acc + cur, 0)
+	$: totalWithBuffer = totalDevDays * 0.85
+	$: totalWithSupportBuffer = support ? totalWithBuffer * 0.95 : totalWithBuffer
+
+</script>
